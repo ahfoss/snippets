a88fa37992813e9740e77b4a57f8a89590702928
ahfoss <alexanderhfoss@gmail.com>
2017-03-09 19:51:17 -0500

    Add disk usage utility

--- bash/doc.sh
+++ bash/doc.sh
@@ -4,3 +4,6 @@ grep -rnw './path' -e 'pattern'
 
 # Same but ignore case and wildcard after pattern
 grep -rnwi './path' -e 'pattern.*'
+
+# recursive directory size to maxdepth 3, -h gives human readable
+du -h -d3
