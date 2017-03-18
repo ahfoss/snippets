530e38f9e57da40475a34a6426a71f24910bc800
ahfoss <alexanderhfoss@gmail.com>
2017-03-18 17:02:58 -0400

    Added way to convert google photos to pdf

--- bash/doc.sh
+++ bash/doc.sh
@@ -7,3 +7,11 @@ grep -rnwi './path' -e 'pattern.*'
 
 # recursive directory size to maxdepth 3, -h gives human readable
 du -h -d3
+
+#####################
+# Convert zip file of images from google photos to reasonable pdf
+unzip Photos.zip
+convert -compress jpeg -quality 25 -rotate 90 -page A4 20170317_13* output.pdf
+# alternate version if RAM is overloaded (much slower though):
+convert -limit memory 1 -limit map 1 -compress jpeg -quality 25 -rotate 90 -page A4 20170317_13* output.pdf 
+
